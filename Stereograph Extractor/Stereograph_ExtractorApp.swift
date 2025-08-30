da353297e83355511fcc418252a3d9e6e719742b
Hal Mueller <halmueller@gmail.com>
2025-08-30 14:11:55 -0700

    Initial Commit

---
+++ Stereograph Extractor/Stereograph_ExtractorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stereograph_ExtractorApp.swift
+//  Stereograph Extractor
+//
+//  Created by Hal Mueller on 8/30/25.
+//
+
+import SwiftUI
+
+@main
+struct Stereograph_ExtractorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
